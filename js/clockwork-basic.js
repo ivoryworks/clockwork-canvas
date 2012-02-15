6018b69f054c0d82930271286eb14f40a54b22d3
ivoryworks <ivoryworkz@gmail.com>
2012-02-15 14:45:52 +0900

    Basic clock(Sweep & Step) add.

---
+++ js/clockwork-basic.js
@@ -0,0 +1,40 @@
+var BasicClock = function(context, offsetX, offsetY, size, type) {
+    this.ctx = context;
+    this.offset = {'x':offsetX, 'y':offsetY};
+    this.cPos = {'x':(offsetX + size/2), 'y':(offsetY + size/2)};
+    this.radius = (size/2)*.98;
+    this.size = size;
+    this.type = (type == undefined) ? 0 : type;
+
+    this.draw = function(date) {
+        with (this) {
+            ClockUtil.clearCanvas(ctx, offset.x, offset.y, offset.x+size, offset.y+size);
+            drawMovement(date);
+        }
+    };
+
+    this.drawMovement = function(date) {
+        with (this) {
+            ClockUtil.drawCircle(ctx, cPos.x, cPos.y, radius);
+            // Second
+            var sec = {'radian':0, 'x':0, 'y':0, 'radius':radius/8};
+            sec.radian = (type == 1) ? ClockUtil.getSecondsRadian(date) : ClockUtil.getSecondsSweepRadian(date);
+            sec.x = cPos.x + (radius - sec.radius) * Math.cos(sec.radian);
+            sec.y = cPos.y - (radius - sec.radius) * Math.sin(sec.radian);
+
+            // Minute
+            var min = {'radian':ClockUtil.getMinutesRadian(date), 'x':0, 'y':0, 'radius':radius/4};
+            min.x = cPos.x + (radius - min.radius) * Math.cos(min.radian);
+            min.y = cPos.y - (radius - min.radius) * Math.sin(min.radian);
+
+            // Hour
+            var hur = {'radian':ClockUtil.getHoursRadian(date), 'x':0, 'y':0, 'radius':radius/2};
+            hur.x = cPos.x + (radius - hur.radius) * Math.cos(hur.radian);
+            hur.y = cPos.y - (radius - hur.radius) * Math.sin(hur.radian);
+
+            ClockUtil.drawLine(ctx, cPos.x, cPos.y, hur.x, hur.y);
+            ClockUtil.drawLine(ctx, cPos.x, cPos.y, min.x, min.y);
+            ClockUtil.drawLine(ctx, cPos.x, cPos.y, sec.x, sec.y);
+        }
+    };
+};
---
+++ js/clockwork-util.js
@@ -0,0 +1,41 @@
+var ClockUtil = function() {};
+ClockUtil.clearCanvas = function(ctx, x1, y1, x2, y2) {
+    with (ctx) {
+        beginPath();
+        clearRect(x1, y1, x2, y2);
+        stroke();
+    }
+};
+
+ClockUtil.drawLine = function(ctx, srcX, srcY, dstX, dstY) {
+    with (ctx) {
+        beginPath();
+        moveTo(srcX, srcY);
+        lineTo(dstX, dstY);
+        stroke();
+    }
+};
+
+ClockUtil.drawCircle = function(ctx, posX, posY, radius) {
+    with (ctx) {
+        beginPath();
+        arc(posX, posY, radius, 0, Math.PI*2, false);
+        stroke();
+    }
+};
+
+ClockUtil.getSecondsSweepRadian = function(date) {
+    return Math.PI * 2 * (15 - (date.getSeconds() + date.getMilliseconds() / 1000)) / 60;
+};
+
+ClockUtil.getSecondsRadian = function(date) {
+    return Math.PI * 2 * (15 - date.getSeconds()) / 60;
+};
+
+ClockUtil.getMinutesRadian = function(date) {
+    return Math.PI * 2 * (15 - (date.getMinutes() + date.getSeconds() / 60)) / 60;
+};
+
+ClockUtil.getHoursRadian = function(date) {
+    return Math.PI * 2 * (3 - (date.getHours() + date.getMinutes() / 60)) / 12;
+}
